51ecb40d8b4988478b1dadbf2b4e0f63146759e7
帅帅的磊哥1 <458820152@qq.com>
2026-03-30 10:42:29 +0800

    build: 添加.gitignore并更新package.json依赖
    feat: 重构server.js导出app模块

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`服务器运行在 http://localhost:${PORT}`);
-  console.log(`API文档: http://localhost:${PORT}`);
-});
+module.exports = app;
